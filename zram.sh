55c245cbc137e6b98dbc1fee3f02478f9c956aae
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2021-06-27 22:23:51 +0000

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-modprobe zram num_devices=4
-
-totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem)* 1024 * 3))
-
-echo $mem > /sys/block/zram0/disksize
-
-mkswap /dev/zram0
-
-swapon -p 5 /dev/zram0
+cpu=$(nproc)
+modprobe zram num_devices=$cpu
+total=$(LANG=C free | grep -e "^Mem" | xargs bash -c 'echo $1')
+mem=$(($total*1024))
+for ((i=0;i<$cpu;i++)) ; do
+    echo $mem > /sys/block/zram$i/disksize
+    mkswap /dev/zram$i
+    swapon -p 5 /dev/zram$i
+done
